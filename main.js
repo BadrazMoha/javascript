49877fbbbb5356996ba615b46920757ad5410305
Badria <badrazmoha79@gmail.com>
2023-02-07 09:27:31 -0800

    add

---
+++ main.js
@@ -0,0 +1,21 @@
+//learning console
+console.log("hellow java script");
+
+// leaning variable
+// string , num,float,boolean
+
+let nam = 'badraz moha';
+let age = 2002;
+const year = 2023
+
+console.log (year - age );
+
+// operaters 
+// assigment opertor =
+
+let x = 5;
+let y = 6;
+
+x+=1;
+
+console.log (x);
