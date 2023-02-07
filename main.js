2072324650ca80e1a66a4624f5850277e8d0d93b
Badria <badrazmoha79@gmail.com>
2023-02-07 10:22:16 -0800

    add

--- main.js
+++ main.js
@@ -4,18 +4,77 @@ console.log("hellow java script");
 // leaning variable
 // string , num,float,boolean
 
-let nam = 'badraz moha';
-let age = 2002;
-const year = 2023
+// let nam = 'badraz moha';
+// let age = 2002;
+// const year = 2023
 
-console.log (year - age );
+// console.log (year - age );
 
-// operaters 
-// assigment opertor =
+// // operaters 
+// // assigment opertor =
 
-let x = 5;
-let y = 6;
+// let x = 5;
+// let y = 6;
+// let mod = 8 % 8;
 
-x+=1;
+// x+=1;
 
-console.log (x);
+// console.log (x);
+
+// arrthimetic operator 
+
+// let y=5; 
+// let h = 8;
+
+// let re = y*h;
+
+// console.log(re);
+
+
+// coparison operator
+
+// let var1 = 6;
+// let var2 = 7;
+
+
+// console .log ( 6 !== var2 );
+
+// // logical operator
+
+// let  var3 = true && true ;
+// let  var4 = false || true ;
+
+// console.log (var3);
+
+// control folow 
+//// if else
+
+// let per = 50;
+
+// if (per > 90){
+//     console.log("A+");
+// }else if(per > 80) {
+//     console.log("B+");
+// }else if(per > 70) {
+//     console.log("c+");
+// }else if(per > 60) {
+//     console.log("d+");
+// } else {
+//     console.log("wax soo baro walaalow");
+// }
+
+/// control folow 
+//// swich
+const expr = 'hhh';
+
+switch(expr){
+    case "apple":
+    console.log("apples are $6.5");
+    break;
+    case "mango" : 
+    console.log("mangoes are $55.5");
+    break;
+     default:
+        console.log("sorry we have not that")
+
+}
